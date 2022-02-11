fd304bd4c0b6413931f807539b405f871f9b97cb
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-02-11 09:13:49 +0000

    feat: convert webm video to mp4

--- apps/server/video/views.py
+++ apps/server/video/views.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+
+import ffmpeg
+from django.conf import settings
 from django.http import JsonResponse
 from rest_framework.views import APIView
 
@@ -6,8 +10,13 @@ from .models import VideoUpload
 
 class VideoUploadView(APIView):
     def post(self, request):
-        video = request.FILES["file"]
-        video.name = "file.webm"
-        vu = VideoUpload(video_file=video)
+        file_obj = request.data["file"]
+        file_obj.name = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.webm"
+        vu = VideoUpload(video_file=file_obj)
+        vu.save()
+        ffmpeg.input(vu.video_file.path).output(f"{vu.video_file.path}.mp4").run()
+        vu.video_file.name = (
+            f"{vu.video_file.name.replace(str(settings.MEDIA_ROOT), '')}.mp4"
+        )
         vu.save()
         return JsonResponse({"id": vu.id})
