97286a1c86455c9f6ad1c3d111a11e394a6fdb22
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-02-18 06:42:29 +0000

    fix: avoid writing images to temp files

--- apps/server/video/emotion_recognition.py
+++ apps/server/video/emotion_recognition.py
@@ -1,7 +1,4 @@
-import contextlib
 import json
-import os
-import tempfile
 
 import cv2
 import numpy as np
@@ -17,32 +14,16 @@ class NumpyEncoder(json.JSONEncoder):
         return json.JSONEncoder.default(self, obj)
 
 
-def video_to_frames(pathIn, unique_id):
-    vid_cap = cv2.VideoCapture(pathIn)
+def process_video(location, unique_id):
+    vid_cap = cv2.VideoCapture(location)
     success, image = vid_cap.read()
     count = 0
-    paths = []
-    while success:
-        path = os.path.join(
-            tempfile.gettempdir(), f"image-{unique_id}-frame-{count}.jpg"
-        )
-        if not cv2.imwrite(path, image):
-            raise Exception("Could not write image")
-        paths.append(path)
-        success, image = vid_cap.read()
-        count += 1
-    return paths
-
-
-def process_video(location, unique_id):
-    paths = video_to_frames(location, unique_id)
     data = []
-    for path in paths:
-        img = cv2.imread(path)
-        detector = FER(mtcnn=True)
-        d = detector.detect_emotions(img)
-        if len(d) > 0:
+    while success:
+        detector = FER()
+        d = detector.detect_emotions(image)
+        success, image = vid_cap.read()
+        if success and len(d) > 0:
             data.append(d)
-        with contextlib.suppress(FileNotFoundError):
-            os.remove(path)
+        count += 1
     return json.loads(json.dumps(data, cls=NumpyEncoder))
