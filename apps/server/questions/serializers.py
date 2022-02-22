56c17154593e9434dae47caddd077978862ad865
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-02-22 06:57:12 +0000

    feat: change serializer

--- apps/server/questions/serializers.py
+++ apps/server/questions/serializers.py
@@ -1,15 +1,34 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from . import models
 
 
-class QuestionSetSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = models.QuestionSet
-        fields = "__all__"
+        model = get_user_model()
+        fields = ["username", "id", "email", "first_name", "last_name"]
 
 
 class QuestionSerializer(serializers.ModelSerializer):
+    answer_duration = serializers.SerializerMethodField()
+    thinking_duration = serializers.SerializerMethodField()
+
     class Meta:
         model = models.Question
+        exclude = ["question_set"]
+
+    def get_answer_duration(self, obj):
+        return obj.answer_duration.total_seconds()
+
+    def get_thinking_duration(self, obj):
+        return obj.thinking_duration.total_seconds()
+
+
+class QuestionSetSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    question_set = QuestionSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = models.QuestionSet
         fields = "__all__"
--- apps/server/questions/views.py
+++ apps/server/questions/views.py
@@ -1,6 +1,4 @@
-from django.forms.models import model_to_dict
 from rest_framework import viewsets
-from rest_framework.response import Response
 
 from . import models, serializers
 
@@ -8,15 +6,6 @@ from . import models, serializers
 class QuestionSetViewSet(viewsets.ModelViewSet):
     queryset = models.QuestionSet.objects.all()
     serializer_class = serializers.QuestionSetSerializer
-
-    def retrieve(self, request, pk=None):
-        question_set = models.QuestionSet.objects.get(id=pk)
-        questions = models.Question.objects.filter(question_set_id=pk)
-        response = model_to_dict(question_set)
-        response["questions"] = serializers.QuestionSerializer(
-            questions, many=True
-        ).data
-        return Response(response)
 
 
 class QuestionViewSet(viewsets.ModelViewSet):
